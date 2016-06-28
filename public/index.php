865ee13de3adbe3ea8bea48b58e7a96b61f32409
PoeticBliss <meremezeukah@gmail.com>
2016-06-28 17:41:52 -0400

    changed file path

--- public/index.php
+++ public/index.php
@@ -8,12 +8,12 @@ if (isset($_GET['url'])) {
   $reqType = strtolower($_SERVER['REQUEST_METHOD']);
   $url = $_GET['url'];   // assign url variable
   $route = new Route($url, $reqType);
-  include 'App/routes.php';
+  include '../App/routes.php';
 } else {
   $url = '/';
   $reqType = strtolower($_SERVER['REQUEST_METHOD']);
   $route = new Route($url, $reqType);
-  include 'App/routes.php';
+  include '../App/routes.php';
 }
 
  ?>
