9c9ba5531f3ddcd95f820c2904c885a7830e5d3b
PoeticBliss <meremezeukah@gmail.com>
2016-06-29 15:44:12 -0400

    updated routes to include defaults

--- App/routes.php
+++ App/routes.php
@@ -4,6 +4,10 @@ $route->get('/user/:user/', function($user) {
  echo "I got your username: ". $user;
 });
 
+$route->get('/', function () {
+  echo 'This is homepage';
+});
+
 $route->get('/user/:user/:id', function($user, $id) {
  echo "I got your username: ". $user ." and id: ". $id;
 });
