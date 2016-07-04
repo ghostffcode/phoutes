ad0fcb50f63ef7ad28a35909475c49c9827ff68f
PoeticBliss <meremezeukah@gmail.com>
2016-07-04 07:46:51 -0400

    added views

--- App/routes.php
+++ App/routes.php
@@ -2,14 +2,15 @@
 
 $route->get('/user/:user/', function($user) {
  echo "I got your username: ". $user;
+ view::render();
 });
 
 $route->get('/', function () {
   echo 'This is homepage';
 });
 
-$route->get('/user/:user/:id', function($user, $id) {
- echo "I got your username: ". $user ." and id: ". $id;
+$route->get('/user/:id', function($user, $id) {
+ echo "I got your id: ". $id;
 });
 
 $route->get('/home', function() {
