3d3c298cfa72c7eb643b1f1071352f2ff15363a9
PoeticBliss <meremezeukah@gmail.com>
2016-07-04 08:35:05 -0400

    sample profile controller

--- App/Controllers/Profile.php
+++ App/Controllers/Profile.php
@@ -6,10 +6,6 @@
  */
 class Profile {
 
-  function __construct() {
-    echo "I am Profile Controller </br >";
-  }
-
   public function name() {
     echo "I am name from profile controller";
   }
