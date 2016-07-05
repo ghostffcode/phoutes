9eb03e4ba4eac39937a6e36a90fb74f40bd44498
PoeticBliss <meremezeukah@gmail.com>
2016-07-05 10:40:37 -0400

    added controller instance

--- App/routes.php
+++ App/routes.php
@@ -2,23 +2,26 @@
 
 $route->get('/user/:user/', function($user) {
  echo "I got your username: ". $user;
- view::render();
-});
-
-$route->get('/', function () {
-  echo 'This is homepage';
-});
-
-$route->get('/user/:id', function($user, $id) {
- echo "I got your id: ". $id;
+ //view::render();
 });
 
 $route->get('/home', function() {
  echo "I got Home <br />";
 });
 
-$route->post('/', function() {
- echo "Post request to homepage";
+$route->get('/', function () {
+  echo 'This is homepage <br />';
+  $profile = new ctrlProfile();
+  $profile->name();
+});
+
+$route->get('/user/:id', function($user, $id) {
+ echo "I got your id: ". $id;
+});
+
+
+$route->Error404(function () {
+  view::render('404');
 });
 
  ?>
