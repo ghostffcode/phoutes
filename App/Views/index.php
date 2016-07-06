f9b60970b7769ed7c6fb0f3c958fd74bec36ed59
PoeticBliss <meremezeukah@gmail.com>
2016-07-05 23:05:22 -0400

    changed assets path

--- App/Views/index.php
+++ App/Views/index.php
@@ -1,12 +1,12 @@
 <html>
   <head>
-    <link rel="stylesheet" href="assets/style.css">
+    <link rel="stylesheet" href="/assets/style.css">
   </head>
   <body>
     <div class="phoutes">
       <div class="phoute-text"><span style="text-align: center;">Phoutes v1.0</span></div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
-    <script src="assets/script.js"></script>
+    <script src="/assets/script.js"></script>
   </body>
 </html>
