f93b80209e9be6e6ca17c8217005021362c9f304
PoeticBliss <meremezeukah@gmail.com>
2016-07-23 11:05:37 -0400

    updated versions

--- App/Views/index.php
+++ App/Views/index.php
@@ -4,7 +4,7 @@
   </head>
   <body>
     <div class="phoutes">
-      <div class="phoute-text"><span style="text-align: center;">Phoutes v1.0</span></div>
+      <div class="phoute-text"><span style="text-align: center;">Phoutes v1.1</span></div>
     </div>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
     <script src="/assets/script.js"></script>
